cd3c980e6555214ebb4d5634ada76f58d26089e2
José Luis <joseluis@MacBook-Air-de-esperanza.local>
2019-10-07 02:19:30 +0200

    Added frameworks, colors and fonts

---
+++ Marvel/Extensions/UIColorExtension.swift
@@ -0,0 +1,25 @@
+//
+//  UIColorExtension.swift
+//  Marvel
+//
+//  Created by José Luis on 07/10/2019.
+//  Copyright © 2019 carlotaprat. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static let MarvelDark = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1.0)
+    
+    static let MarvelLight = UIColor(red:0.13, green:0.15, blue:0.16, alpha:1.0)
+    
+    static let MarvelRed = UIColor(red:0.91, green:0.27, blue:0.18, alpha:1.0)
+    
+    static let MarvelWhite = UIColor.white
+
+
+
+    
+}
