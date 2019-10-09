74085d4d94bcfc14a19b9384e6f5f41c43394576
José Luis <joseluis@MacBook-Air-de-esperanza.local>
2019-10-09 13:34:32 +0200

    Created new launch screen

--- Marvel/View Controllers/Launch/LaunchViewController.swift
+++ Marvel/View Controllers/Launch/LaunchViewController.swift
@@ -5,6 +5,7 @@ import UIKit
 class LaunchViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var logoImageView: UIImageView!
     
     
     override func viewDidLoad() {
@@ -16,14 +17,21 @@ class LaunchViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        self.imageView.setAnchorPoint(point: CGPoint(x: 0.0, y: 1.0))
+        //self.imageView.setAnchorPoint(point: CGPoint(x: 0.0, y: 1.0))
 
-        UIView.animate(withDuration: 2.5, delay: 0.0, options: .curveEaseIn, animations: {
-            
-            self.imageView.transform = CGAffineTransform.init(scaleX: 1.5, y: 1.5)
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: .curveEaseIn, animations: {
+            self.logoImageView.alpha = 1
+            self.logoImageView.transform = CGAffineTransform.init(scaleX: 2.5, y: 2.5)
+            //self.imageView.transform = CGAffineTransform.init(scaleX: 1.5, y: 1.5)
         }, completion: { (true) in
-            self.performSegue(withIdentifier: "mainsegue", sender: self)
+            UIView.animate(withDuration: 1.0, delay: 0.5, options: .curveEaseOut, animations: {
+                self.imageView.transform = CGAffineTransform.init(translationX: 0.0, y: self.imageView.frame.height - 5)
+            }, completion: { (true) in
+                self.performSegue(withIdentifier: "mainsegue", sender: self)
+            })
         })
+        
+        
     }
 
 }
